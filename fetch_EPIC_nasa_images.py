9d6961b3d35e786a6544431573fbf52ac622719f
Максим Калинин <maksim.kalinin1@gmail.com>
2022-08-02 14:59:11 +0300

    Added optional argument to the function

--- fetch_EPIC_nasa_images.py
+++ fetch_EPIC_nasa_images.py
@@ -8,7 +8,12 @@ from function_for_determining_file_extension import get_file_extension
 from function_for_download_pictures import download_pictures
 
 
-def fetch_epic_nasa_launch(epic_nasa_url, payload, count_photo, path_to_pictures):
+def fetch_epic_nasa_launch(
+    payload,
+    count_photo,
+    path_to_pictures,
+    epic_nasa_url='https://api.nasa.gov/EPIC/api/natural/images',
+     ):
     response = requests.get(epic_nasa_url, params=payload)
     response.raise_for_status()
     for epic_photo_number, epic_id_date in enumerate(response.json()[:count_photo]):
@@ -31,19 +36,29 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="Программа для скачивания эпичных фото земли от NASA"
     )
-    parser.add_argument("url", help="Ссылка на скачивание")
+    parser.add_argument("--url", help="Ссылка на скачивание")
     args = parser.parse_args()
     path_to_pictures = './images/'
     epic_nasa_payload = {
         'api_key': nasa_api_key,
     }
     count_epic_photo = 2
-    try:
-        fetch_epic_nasa_launch(
-            args.url,
-            epic_nasa_payload,
-            count_epic_photo,
-            path_to_pictures
-        )
-    except requests.exceptions.HTTPError as e:
-        print(e.response.status_code)
+    if args.url:
+        try:
+            fetch_epic_nasa_launch(
+                epic_nasa_payload,
+                count_epic_photo,
+                path_to_pictures,
+                args.url,
+            )
+        except requests.exceptions.HTTPError as e:
+            print(e.response.status_code)
+    else:
+        try:
+            fetch_epic_nasa_launch(
+                epic_nasa_payload,
+                count_epic_photo,
+                path_to_pictures,
+            )
+        except requests.exceptions.HTTPError as e:
+            print(e.response.status_code)
