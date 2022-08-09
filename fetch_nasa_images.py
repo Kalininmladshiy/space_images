784aae9e2a0d30c553c9ca4fb0a3759ac01305eb
Максим Калинин <maksim.kalinin1@gmail.com>
2022-08-09 21:08:29 +0300

    Added optional argument to the nasa photo

--- fetch_nasa_images.py
+++ fetch_nasa_images.py
@@ -7,20 +7,24 @@ from function_for_determining_file_extension import get_file_extension
 from function_for_download_pictures import download_pictures
 
 
-def fetch_nasa_launch(url, payload, path_to_pictures):
+def fetch_nasa_launch(
+    path_to_pictures,
+    payload,
+    url='https://api.nasa.gov/planetary/apod',
+     ):
     response = requests.get(url, params=payload)
     response.raise_for_status()
     for photo_number, day_nasa_photo in enumerate(response.json()):
         download_pictures(
-            day_nasa_photo['url'],
             path_to_pictures,
             f'nasa_{photo_number}{get_file_extension(day_nasa_photo["url"])}',
+            day_nasa_photo['url'],
         )
 
 
 if __name__ == '__main__':
     load_dotenv()
-    nasa_api_key = os.environ['NASA_API_KEY']    
+    nasa_api_key = os.environ['NASA_API_KEY']
     path_to_pictures = './images/'
     nasa_payload = {
         'api_key': nasa_api_key,
@@ -29,13 +33,22 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Программа для загрузки изображений NASA'
     )
-    parser.add_argument("url", help="Ссылка для скачивания изображений")
+    parser.add_argument("--url", help="Ссылка для скачивания изображений")
     args = parser.parse_args()
-    try:
-        fetch_nasa_launch(
-            args.url,
-            nasa_payload,
-            path_to_pictures
-        )
-    except requests.exceptions.HTTPError as e:
-        print(e.response.status_code)
+    if args.url:
+        try:
+            fetch_nasa_launch(
+                path_to_pictures,
+                nasa_payload,
+                args.url,
+            )
+        except requests.exceptions.HTTPError as e:
+            print(e.response.status_code)
+    else:
+        try:
+            fetch_nasa_launch(
+                path_to_pictures,
+                nasa_payload,
+            )
+        except requests.exceptions.HTTPError as e:
+            print(e.response.status_code)
