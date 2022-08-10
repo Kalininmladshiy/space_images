c801d0bc3c9c355a004a5a871f710138bad7d82c
Максим Калинин <maksim.kalinin1@gmail.com>
2022-08-10 21:53:29 +0300

    Update space_photos.py

--- space_photos.py
+++ space_photos.py
@@ -7,29 +7,27 @@ from dotenv import load_dotenv
 from fetch_EPIC_nasa_images import fetch_epic_nasa_launch
 from fetch_nasa_images import fetch_nasa_launch
 from fetch_spacex_images import fetch_spacex_launch
-from function_for_download_pictures import download_pictures
+from utils import download_pictures
 
 
 if __name__ == '__main__':
     load_dotenv()
     nasa_api_key = os.environ['NASA_API_KEY']
-    path = Path("/home/maksim/python_projects/space_images/images")
+    path = Path.cwd() / 'images'
+    url="https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
     path.mkdir(parents=True, exist_ok=True)
-    url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
-    path_to_pictures = './images/'
+    path_to_pictures = path
     filename = 'space.jpeg'
     try:
         download_pictures(
-            url,
             path_to_pictures,
             filename,
+            url,
         )
     except requests.exceptions.HTTPError as e:
         print(e.response.status_code)
     flight_id = '5eb87d47ffd86e000604b38a'
     spacex_url = f"https://api.spacexdata.com/v5/launches/"
-    nasa_url = 'https://api.nasa.gov/planetary/apod'
-    epic_nasa_url = 'https://api.nasa.gov/EPIC/api/natural/images'
     nasa_payload = {
         'api_key': nasa_api_key,
         'count': 10
@@ -40,7 +38,6 @@ if __name__ == '__main__':
     count_epic_photo = 2
     try:
         fetch_epic_nasa_launch(
-            epic_nasa_url,
             epic_nasa_payload,
             count_epic_photo,
             path_to_pictures,
@@ -49,9 +46,8 @@ if __name__ == '__main__':
         print(e.response.status_code)
     try:
         fetch_nasa_launch(
-            nasa_url,
-            nasa_payload,
             path_to_pictures,
+            nasa_payload,
         )
     except requests.exceptions.HTTPError as e:
         print(e.response.status_code)
